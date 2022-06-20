04157ca6f10d3ba73b9507571a4ae56913adbe57
devtdq1701 <nnhuyenmyepu1701@gmail.com>
2022-06-20 18:05:23 +0700

    Update Jenkinsfile

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React from "react";Todo App
-import './App.css';
-import TodoList from './components/TodoList';
+import React from "react";
+import "./App.css";
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
-    <div className='todo-app'>
+    <div className="todo-app">
       <TodoList />
-
     </div>
   );
 }
